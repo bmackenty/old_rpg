9dc964b9610e2fcf8dd88f5a089a544aa9b74b9b
Bill MacKenty <bmackenty@users.noreply.github.com>
2023-02-01 15:51:36 +0100

    Create character_wiz.py

---
+++ character_wiz.py
@@ -0,0 +1,6 @@
+character = {
+    1:{"name":"Character 1", "x":15, "y":565},
+    2:{"name":"Character 2", "x":15, "y":595},
+    3:{"name":"Character 3", "x":15, "y":625},
+    4:{"name":"Character 4", "x":15, "y":655}    
+}
